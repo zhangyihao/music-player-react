760ba43c45d9052f8779827e62e7137ff8a1b617
zhangyihao <zyh5540@163.com>
2017-09-11 20:58:34 +0800

    add progressbar component

--- app/components/progress.js
+++ app/components/progress.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './progress.less'
 
 let Progress = React.createClass({
+	getDefaultProps() {
+		return {
+			barColor: '#2f9842'
+		}
+	},
+
+	changeProgress(e) {
+		let progressbar = this.refs.progressbar;
+		//console.log(e.clientX+'-'+progressbar.getBoundingClientRect().left+"-"+progressbar.clientWidth);
+		let progress = (e.clientX - progressbar.getBoundingClientRect().left)/ progressbar.clientWidth;
+		this.props.onProgressChange && this.props.onProgressChange(progress);
+	},
+
 	render() {
 		return (
-			<div className="component-progress row">
-				{ this.props.progress }s
+			<div className="component-progress" ref="progressbar" onClick={this.changeProgress}>
+				<div className="progress" style={{ width: `${this.props.progress}%`,
+				 backgroundColor: `${this.props.barColor}` }}></div>
 			</div>
 		);
 	}
--- app/root.js
+++ app/root.js
@@ -2,12 +2,46 @@ import React from 'react';
 import Header from './components/header'
 import Progress from './components/progress'
 
+let duration = null;
 let Root = React.createClass({
+	getInitialState() {
+		return {
+			progress: '-'
+		}
+	},
+
+	componentDidMount() {
+		$('#player').jPlayer({
+			ready: function() {
+				$(this).jPlayer('setMedia', {
+					mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+				}).jPlayer('play');
+			},
+			supplied: 'mp3',
+			wmode: 'window'
+		});
+		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
+			duration = e.jPlayer.status.duration;
+			this.setState({
+				progress: e.jPlayer.status.currentPercentAbsolute
+			});
+		});
+	},
+
+	componentWillUnMountZ() {
+		$("#player").unbind($.jPlayer.event.timeupdate)
+	},
+
+	progressChangeHandler(progress) {
+		$('#player').jPlayer('play', duration * progress);
+	},
+
 	render() {
 		return (
 			<div>
 			<Header />
-			<Progress progress="1">
+			<Progress progress={ this.state.progress } onProgressChange={this.progressChangeHandler}
+				barColor='#ff0000'>
 			</Progress>
 			</div>
 		);
